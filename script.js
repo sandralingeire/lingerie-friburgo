ff91f95a6d4e239b4b894c7f8d2510c12fb3cebb
Vendas123 <andremonizdearagao@gmail.com>
2025-04-25 21:56:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,68 +2,235 @@ const baseUrl = "imagens/";
 
 const produtos = [
   {
-    nome: 'Calcinha 1',
-    preco: 7.00,
-    tamanhos: ['Único'],
-    cores: ['Preto', 'Vermelho', 'Banco', 'Vinho', 'Rosa'],
-    fotos: [`${baseUrl}calcinha 1.1.webp`, `${baseUrl}calcinha 1.2.webp`, `${baseUrl}calcinha 1.3.webp`]
-  },
-  {
-    nome: 'Calcinha 2',
-    preco: 5.00,
-    tamanhos: ['Único'],
-    cores: ['Rosa', 'Preto', 'Branco', 'Verde', 'Vinho', 'Azul', 'Vermelho'],
-    fotos: [`${baseUrl}calcinha 2.1.webp`, `${baseUrl}calcinha 2.2.webp`, `${baseUrl}calcinha 2.3.webp`, `${baseUrl}calcinha 2.4.webp`, `${baseUrl}calcinha 2.5.webp`, `${baseUrl}calcinha 2.6.webp`, `${baseUrl}calcinha 2.7.webp`, `${baseUrl}calcinha 2.8.webp`]
-  },
-  {
-    nome: 'Camisola',
-    preco: 25.00,
-    tamanhos: ['P', 'M', 'G'],
-    cores: ['Romance', 'Branco com preto', 'Vermelho', 'Preto', 'Azul', 'Caramelo'],
-    fotos: [`${baseUrl}camisola 1.webp`, `${baseUrl}camisola 2.webp`, `${baseUrl}camisola 3.webp`, `${baseUrl}camisola 4.webp`, `${baseUrl}camisola 5.webp`, `${baseUrl}camisola 6.webp`]
-  },
-  {
-    nome: 'Conjunto 1',
-    preco: 22.00,
-    tamanhos: ['P', 'M', 'G'],
-    cores: ['Preto', 'Vermelho', 'Branco', 'Rosa', 'Roxo'],
-    fotos: [`${baseUrl}conjunto 1.1.webp`, `${baseUrl}conjunto 1.2.webp`, `${baseUrl}conjunto 1.3.webp`, `${baseUrl}conjunto 1.4.webp`]
-  },
-  {
-    nome: 'Conjunto 2',
-    preco: 18.00,
-    tamanhos: ['P', 'M', 'G'],
-    cores: ['Preto', 'Vermelho', 'Roxo', 'Banco', 'Azul'],
-    fotos: [`${baseUrl}conjunto 2.1.webp`, `${baseUrl}conjunto 2.2.webp`, `${baseUrl}conjunto 2.3.webp`, `${baseUrl}conjunto 2.4.webp`, `${baseUrl}conjunto 2.5.webp`, `${baseUrl}conjunto 2.6.webp`, `${baseUrl}conjunto 2.7.webp`]
-  },
-  {
-    nome: 'Conjunto 3',
-    preco: 18.00,
-    tamanhos: ['P', 'M', 'G'],
-    cores: ['Rosa', 'Verde', 'Azul', 'Vinho', 'Preto'],
-    fotos: [`${baseUrl}conjunto 3.1.webp`, `${baseUrl}conjunto 3.2.webp`, `${baseUrl}conjunto 3.3.webp`, `${baseUrl}conjunto 3.3.webp`, `${baseUrl}conjunto 3.4.webp`]
-  },
-  {
-    nome: 'Conjunto 4',
-    preco: 30.00,
-    tamanhos: ['P', 'M', 'G'],
-    cores: ['Preto', 'Vermelho', 'Branco', 'Rosa', 'Roxo'],
-    fotos: [`${baseUrl}conjunto 4.1.webp`, `${baseUrl}conjunto 4.2.webp`, `${baseUrl}conjunto 4.3.webp`, `${baseUrl}conjunto 4.4.webp`]
-  },
-  {
-    nome: 'Conjunto 5',
-    preco: 20.00,
-    tamanhos: ['P', 'M', 'G'],
-    cores: ['Rosa', 'Preto', 'Vermelho', 'Branco', 'Roxo'],
-    fotos: [`${baseUrl}conjunto 5.1.webp`, `${baseUrl}conjunto 5.2.webp`, `${baseUrl}conjunto 5.3.webp`, `${baseUrl}conjunto 5.4.webp`, `${baseUrl}conjunto 5.5.webp`]
-  },
-  {
-    nome: 'Conjunto de 3 Peças',
-    preco: 28.00,
-    tamanhos: ['M', 'G', 'GG'],
-    cores: ['Vermelho', 'Preto', 'Roxo', 'Azul', 'Rosa'],
-    fotos: [`${baseUrl}conjunto de 3 peças 1.webp`, `${baseUrl}conjunto de 3 peças 2.webp`, `${baseUrl}conjunto de 3 peças 3.webp`, `${baseUrl}conjunto de 3 peças 4.webp`, `${baseUrl}conjunto de 3 peças 5.webp`, `${baseUrl}conjunto de 3 peças 6.webp`, `${baseUrl}conjunto de 3 peças 7.webp`]
+  nome: 'Calcinha 1',
+  preco: 7.00,
+  fotos: [`${baseUrl}calcinha 1.1.webp`, `${baseUrl}calcinha 1.2.webp`, `${baseUrl}calcinha 1.3.webp`],
+  estoque: {
+    'Único': {
+      'Preto': true,
+      'Vermelho': true,
+      'Banco': true,
+      'Vinho': true,
+      'Rosa': true
+    }
   }
+},
+{
+  nome: 'Calcinha 2',
+  preco: 5.00,
+  fotos: [`${baseUrl}calcinha 2.1.webp`, `${baseUrl}calcinha 2.2.webp`, `${baseUrl}calcinha 2.3.webp`, `${baseUrl}calcinha 2.4.webp`, `${baseUrl}calcinha 2.5.webp`, `${baseUrl}calcinha 2.6.webp`, `${baseUrl}calcinha 2.7.webp`, `${baseUrl}calcinha 2.8.webp`],
+  estoque: {
+    'Único': {
+      'Rosa': true,
+      'Preto': true,
+      'Branco': true,
+      'Verde': true,
+      'Vinho': true,
+      'Azul': true,
+      'Vermelho': true
+    }
+  }
+},
+{
+  nome: 'Camisola',
+  preco: 25.00,
+  fotos: [`${baseUrl}camisola 1.webp`, `${baseUrl}camisola 2.webp`, `${baseUrl}camisola 3.webp`, `${baseUrl}camisola 4.webp`, `${baseUrl}camisola 5.webp`, `${baseUrl}camisola 6.webp`],
+  estoque: {
+    'P': {
+      'Romance': true,
+      'Branco com preto': true,
+      'Vermelho': true,
+      'Preto': true,
+      'Azul': true,
+      'Caramelo': true
+    },
+    'M': {
+      'Romance': true,
+      'Branco com preto': true,
+      'Vermelho': true,
+      'Preto': true,
+      'Azul': true,
+      'Caramelo': true
+    },
+    'G': {
+      'Romance': true,
+      'Branco com preto': true,
+      'Vermelho': true,
+      'Preto': true,
+      'Azul': true,
+      'Caramelo': true
+    }
+  }
+},
+{
+  nome: 'Conjunto 1',
+  preco: 22.00,
+  fotos: [`${baseUrl}conjunto 1.1.webp`, `${baseUrl}conjunto 1.2.webp`, `${baseUrl}conjunto 1.3.webp`, `${baseUrl}conjunto 1.4.webp`],
+  estoque: {
+    'P': {
+      'Preto': true,
+      'Vermelho': true,
+      'Branco': true,
+      'Rosa': true,
+      'Roxo': true
+    },
+    'M': {
+      'Preto': true,
+      'Vermelho': true,
+      'Branco': true,
+      'Rosa': true,
+      'Roxo': true
+    },
+    'G': {
+      'Preto': true,
+      'Vermelho': true,
+      'Branco': true,
+      'Rosa': true,
+      'Roxo': true
+    }
+  }
+},
+{
+  nome: 'Conjunto 2',
+  preco: 18.00,
+  fotos: [`${baseUrl}conjunto 2.1.webp`, `${baseUrl}conjunto 2.2.webp`, `${baseUrl}conjunto 2.3.webp`, `${baseUrl}conjunto 2.4.webp`, `${baseUrl}conjunto 2.5.webp`, `${baseUrl}conjunto 2.6.webp`, `${baseUrl}conjunto 2.7.webp`],
+  estoque: {
+    'P': {
+      'Preto': true,
+      'Vermelho': true,
+      'Roxo': true,
+      'Banco': true,
+      'Azul': true
+    },
+    'M': {
+      'Preto': true,
+      'Vermelho': true,
+      'Roxo': true,
+      'Banco': true,
+      'Azul': true
+    },
+    'G': {
+      'Preto': true,
+      'Vermelho': true,
+      'Roxo': true,
+      'Banco': true,
+      'Azul': true
+    }
+  }
+},
+{
+  nome: 'Conjunto 3',
+  preco: 18.00,
+  fotos: [`${baseUrl}conjunto 3.1.webp`, `${baseUrl}conjunto 3.2.webp`, `${baseUrl}conjunto 3.3.webp`, `${baseUrl}conjunto 3.4.webp`],
+  estoque: {
+    'P': {
+      'Rosa': true,
+      'Verde': true,
+      'Azul': true,
+      'Vinho': true,
+      'Preto': true
+    },
+    'M': {
+      'Rosa': true,
+      'Verde': true,
+      'Azul': true,
+      'Vinho': true,
+      'Preto': true
+    },
+    'G': {
+      'Rosa': true,
+      'Verde': true,
+      'Azul': true,
+      'Vinho': true,
+      'Preto': true
+    }
+  }
+},
+{
+  nome: 'Conjunto 4',
+  preco: 30.00,
+  fotos: [`${baseUrl}conjunto 4.1.webp`, `${baseUrl}conjunto 4.2.webp`, `${baseUrl}conjunto 4.3.webp`, `${baseUrl}conjunto 4.4.webp`],
+  estoque: {
+    'P': {
+      'Preto': true,
+      'Vermelho': true,
+      'Branco': true,
+      'Rosa': true,
+      'Roxo': true
+    },
+    'M': {
+      'Preto': true,
+      'Vermelho': true,
+      'Branco': true,
+      'Rosa': true,
+      'Roxo': true
+    },
+    'G': {
+      'Preto': true,
+      'Vermelho': true,
+      'Branco': true,
+      'Rosa': true,
+      'Roxo': true
+    }
+  }
+},
+{
+  nome: 'Conjunto 5',
+  preco: 20.00,
+  fotos: [`${baseUrl}conjunto 5.1.webp`, `${baseUrl}conjunto 5.2.webp`, `${baseUrl}conjunto 5.3.webp`, `${baseUrl}conjunto 5.4.webp`, `${baseUrl}conjunto 5.5.webp`],
+  estoque: {
+    'P': {
+      'Rosa': true,
+      'Preto': true,
+      'Vermelho': true,
+      'Branco': true,
+      'Roxo': true
+    },
+    'M': {
+      'Rosa': true,
+      'Preto': true,
+      'Vermelho': true,
+      'Branco': true,
+      'Roxo': true
+    },
+    'G': {
+      'Rosa': true,
+      'Preto': true,
+      'Vermelho': true,
+      'Branco': true,
+      'Roxo': true
+    }
+  }
+},
+{
+  nome: 'Conjunto de 3 Peças',
+  preco: 28.00,
+  fotos: [`${baseUrl}conjunto de 3 peças 1.webp`, `${baseUrl}conjunto de 3 peças 2.webp`, `${baseUrl}conjunto de 3 peças 3.webp`, `${baseUrl}conjunto de 3 peças 4.webp`, `${baseUrl}conjunto de 3 peças 5.webp`, `${baseUrl}conjunto de 3 peças 6.webp`, `${baseUrl}conjunto de 3 peças 7.webp`],
+  estoque: {
+    'M': {
+      'Vermelho': true,
+      'Preto': true,
+      'Roxo': true,
+      'Azul': true,
+      'Rosa': true
+    },
+    'G': {
+      'Vermelho': true,
+      'Preto': true,
+      'Roxo': true,
+      'Azul': true,
+      'Rosa': true
+    },
+    'GG': {
+      'Vermelho': true,
+      'Preto': true,
+      'Roxo': true,
+      'Azul': true,
+      'Rosa': true
+    }
+  }
+}
+
 ];
 
 
